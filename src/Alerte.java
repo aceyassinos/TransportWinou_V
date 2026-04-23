5e2f351e5da3555f6464d3d9e8599cb8cd8155fe
Aziz-033 <azizzkorri3@gmail.com>
2026-04-23 23:38:21 +0100

    Add Alerte class with properties and methods Mohamed aziz korri

--- src/Alerte.java
+++ src/Alerte.java
@@ -1,1 +1,33 @@
+public class Alerte {
+    private int id;
+    private String type;
+    private String message;
 
+    public Alerte(int id, String type, String message) {
+        this.id = id;
+        this.type = type;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void envoyerAlerte() {
+        System.out.println("Alerte : " + type + " - " + message);
+    }
+
+    public void afficherDetails() {
+        System.out.println("ID : " + id);
+        System.out.println("Type : " + type);
+        System.out.println("Message : " + message);
+    }
+}
